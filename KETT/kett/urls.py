abb29d2a6faf225137fa7fc3373c26f1800fd67d
UnoYakshi <yellowwanna@gmail.com>
2019-09-22 21:25:33 +0300

    Renamed KETT to kett...

--- KETT/kett/urls.py
+++ KETT/kett/urls.py
@@ -1,4 +1,4 @@
-"""KETT URL Configuration
+"""kett URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
